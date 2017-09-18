cd591c025a2f793092617df885e3ae6fbb548f16
Christopher Meigs <cjmeigs017@gmail.com>
2017-09-18 15:06:57 -0400

    Completed W4 D1 pair exercise.

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,5 +1,21 @@
 package com.techelevator;
 
 public class KataStringCalculator {
-
+	
+	
+	public int add(String numbers) {
+		int answer = 0;
+		if(numbers == null || numbers.length() == 0) {
+			return answer;
+		} else {
+		String [] parts = numbers.split("[\\s\\D]+");
+		for(String part : parts) {
+			if(part.equals("")) {
+				continue;
+			}
+			answer += Integer.parseInt(part);
+		}
+		return answer;
+		}
+	}
 }
--- m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -5,4 +5,58 @@ import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
 
+	KataStringCalculator sut;
+	
+	@Before
+	 public void setUp () {
+		sut = new KataStringCalculator();
+	}
+	
+	@Test
+	public void testIntAddSimple() {
+		int result = sut.add("1,2");
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void testIntNull() {
+		int result = sut.add(null);
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testIntAddToEight() {
+		int result = sut.add("1,2,5");
+		assertEquals(8, result);
+	}
+	
+	@Test
+	public void testIntEmpty() {
+		int result = sut.add("");
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testIntAddToSix() {
+		int result = sut.add("1\n2,3");
+		assertEquals(6, result);
+	}
+	
+	@Test
+	public void testIntDiffDelims() {
+		int result = sut.add("1;2 , 7\n10,3");
+		assertEquals(23, result);
+	}
+	
+	@Test
+	public void testIntThree() {
+		int result = sut.add("//;\n1;2");
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void testIntThirteen() {
+		int result = sut.add("//!\n4!9");
+		assertEquals(13, result);
+	}
 }
