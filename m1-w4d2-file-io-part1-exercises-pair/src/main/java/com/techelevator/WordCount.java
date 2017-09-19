79aac5cb2f66001aaa520db502b0a9d2c47764bd
Christopher Meigs <cjmeigs017@gmail.com>
2017-09-19 13:55:23 -0400

    Completed W4 D2 Pair project.

--- m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
+++ m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
@@ -7,8 +7,35 @@ import java.util.Scanner;
 
 public class WordCount {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception{
+		
+		File inputFile = new File("/Users/christophermeigs/workspace/exercises/m1-w4d2-file-io-part1-exercises/alices_adventures_in_wonderland.txt");
+		System.out.println(inputFile.exists());
+		
+		try(Scanner input = new Scanner(inputFile)) {
+			int answer = 0;
+			while(input.hasNextLine()) {
+				String line = input.nextLine();
+				String [] parts = line.split("[\\!\\.\\?]");
+					answer += parts.length - 1;
+					}
+					System.out.println(answer);
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+		
+		try(Scanner inputW = new Scanner(inputFile)) {
+			int answer = 0;
+			while(inputW.hasNextLine()) {
+				String line = inputW.nextLine();
+				String [] parts = line.split("[\\s\\d\\W]+");
+					answer += parts.length - 1;
+					}
+					System.out.println(answer);
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+	
 	}
-
 }
+
