f29d32feabb2e1826efccb0bb74fb7f5a02cd4ee
Christopher Meigs <cjmeigs017@gmail.com>
2017-09-20 14:37:50 -0400

    Completed W4 D3 Pair project.

--- m1-w4d3-file-io-part2-exercises-pair/src/main/java/com/techelevator/FindAndReplace.java
+++ m1-w4d3-file-io-part2-exercises-pair/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,10 +1,50 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
 	public static void main(String[] args) {
 		
-
+		Scanner inputWord = new Scanner(System.in);
+		System.out.println("Please type in a word >>> ");
+		String userInput1 = inputWord.nextLine();
+		
+		System.out.println("Please type in a replacement word >>> ");
+		String userInput2 = inputWord.nextLine();
+		
+		System.out.println("Please type in a file name >>> ");
+		String userInput3 = inputWord.nextLine();
+		
+		System.out.println("Please type in a destination file >>> ");
+		String userInput4 = inputWord.nextLine();
+		
+		File originalAaiw = new File(userInput3);
+		File writeOverAaiw = new File(userInput4);
+		System.out.println(originalAaiw.exists());
+		
+		try(
+				Scanner newPom = new Scanner(originalAaiw); 
+				PrintWriter writePom = new PrintWriter(writeOverAaiw)
+		) {
+			while(newPom.hasNextLine()) {
+				String line = newPom.nextLine();
+				if(line.contains(userInput1)) {
+					line = line.replace(userInput1, userInput2);
+				}
+				
+				writePom.println(line);
+			}
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("This is not a file. Try again.");
+			System.exit(1);
+		} catch(Exception f) {
+			System.out.println("Invalid destination. Please try again.");
+			System.exit(1);
+		}
 	}
-
 }
